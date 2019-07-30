b13fbb786e76df5155e289e243905c5d50bfac9e
Brad Voris <40213182+bvoris@users.noreply.github.com>
2019-07-30 09:03:53 -0500

    Add files via upload

---
+++ SID from Name or Group.ps1
@@ -0,0 +1,5 @@
+﻿#Determine SID from user or group
+#Change name of user or group in $name variable
+import-module activedirectory
+$name = “DomainName\Groupname”
+(New-Object System.Security.Principal.NTAccount($name)).Translate([System.Security.Principal.SecurityIdentifier]).value
